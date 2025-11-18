c72968017dc6e57cc90f3dcf84151cbbe40aa31b
Thomas F. Varley <contact@thosvarley.me>
2025-11-18 08:46:27 -0500

    Mixed datatype estimators

---
+++ syntropy/mixed/__init__.py
@@ -0,0 +1,11 @@
+from .shannon import (
+        shannon_entropy, 
+        conditional_entropy, 
+        mutual_information
+        )
+
+__all__ = [
+        "shannon_entropy",
+        "conditional_entropy",
+        "mutual_information"
+        ]
